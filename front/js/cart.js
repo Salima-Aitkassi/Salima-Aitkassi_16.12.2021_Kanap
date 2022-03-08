2c1316f1b102dfc5c7352d92becde41e3172b147
Salima Ait Kassi <salyma.salama@gmail.com>
2022-03-08 15:29:51 +0100

    page confirmation terminée

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,13 +1,3 @@
-//Recuperer la session
-// recuperer le panier depuis la session getItem 
-// Verifier si le panier est null ( reccuper le getelementby id la ou je veux afficher le message ) 
-// a
-//foreach sur le panier pour chaque produit avec lid sauvegardé dans la session 
-// appel à l'api pour recup chaque produit a partir de son id cf product.js 
-
-
-
-
 //------- Recuperation du panier ------------------------
 productsInLocalStorage = JSON.parse(localStorage.getItem("panier"))
 //console.log(productsInLocalStorage)
